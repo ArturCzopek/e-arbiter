dd154fd6ce5bc9a439ec129f2b513ccae0426381
Maciej Marczak <maciejmarczak@hotmail.com>
2017-05-26 14:01:10 +0200

    fixed whitespaces in python script

--- e-arbiter-executor/src/main/resources/docker_setup/run.py
+++ e-arbiter-executor/src/main/resources/docker_setup/run.py
@@ -14,48 +14,50 @@ prog = "program"
 test_data = open("test_data", "r");
 
 lang_commands = [
-	# language, compile command, execute command
-	(".c", "gcc -o {prog} {prog}.c".format(prog=prog), "./{}".format(prog))
+    # language, compile command, execute command
+    (".c", "gcc -o {prog} {prog}.c".format(prog=prog), "./{}".format(prog))
 ]
 
 program = commands.getstatusoutput("find . -name '{}*'".format(prog))[1]
 
+
 def prepare_command(execute_command, args):
-	cmd = execute_command
-	for arg in args:
-		cmd += ' "{}"'.format(arg)
-	return cmd
+    cmd = execute_command
+    for arg in args:
+        cmd += ' "{}"'.format(arg)
+    return cmd
+
 
 # find commands tuple for this extension
 for tuple in lang_commands:
-	if program.endswith(tuple[0]):
-		ext, compile_command, execute_command = tuple
+    if program.endswith(tuple[0]):
+        ext, compile_command, execute_command = tuple
 
 # compile
 status, output = commands.getstatusoutput(compile_command)
 if status != 0:
-	print output
-	sys.exit()
+    print output
+    sys.exit()
 
 iterations = 0
 successes = 0
 
 # execute
 for line in test_data.readlines():
-	args = shlex.split(line)
-	expected = args.pop()
-	
-	status, output = commands.getstatusoutput(prepare_command(execute_command, args))
+    args = shlex.split(line)
+    expected = args.pop()
 
-	result = "EXPECTED: {} | ACTUAL: {} | PASSED: {}".format(expected, output, output == expected)
+    status, output = commands.getstatusoutput(prepare_command(execute_command, args))
 
-	if output == expected:
-		successes += 1
-	iterations += 1
+    result = "EXPECTED: {} | ACTUAL: {} | PASSED: {}".format(expected, output, output == expected)
 
-	print result
+    if output == expected:
+        successes += 1
+    iterations += 1
+
+    print result
 
 if successes == iterations:
-	print "ALL PASSED"
+    print "ALL PASSED"
 else:
-	print "{} FAILED".format(iterations - successes)
+    print "{} FAILED".format(iterations - successes)
