db5d04fa9b25ab5b412af0a740cbc52b75b36620
Artur Czopek <arturcz32@gmail.com>
2017-07-08 19:33:17 +0200

    #55 - Fixed PR condition (do script if it's a master branch and NOT PR)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ cd $TRAVIS_BUILD_DIR
 
 echo "Branch: "$TRAVIS_BRANCH" pull request: "$TRAVIS_PULL_REQUEST
 
-if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST != 'false' ]]
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]
 then
   echo "Start sonar analysis"
   ./gradlew sonarqube -Dsonar.host.url=https://sonarqube.com -Dsonar.organization=cyganki -Dsonar.login=$SONAR_TOKEN
