847046b2dd529ae3356d76d1464092d15f1b1319
Artur Czopek <arturcz32@gmail.com>
2017-07-09 18:48:28 +0200

    - Fixed build script (invalid builds sometimes looks like "everything is ok, I just not compile!")

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,30 @@
 cd $TRAVIS_BUILD_DIR
 
 echo "Build script for e-Arbiter"
-echo "Branch: "$TRAVIS_BRANCH" pull request: "$TRAVIS_PULL_REQUEST
+echo "Branch: "$TRAVIS_BRANCH", pull request: "$TRAVIS_PULL_REQUEST
 
-./gradlew build
-cd e-arbiter-web
-ng build --env=dev --base-href "https://arturczopek.github.io/e-arbiter/" -aot
+gradlew_output=$(./gradlew build); gradlew_return_code=$?
+
+if (( gradlew_return_code != 0 ))
+then
+    echo "!! Gradle build - FAIL !!";
+    exit 1;
+else
+    echo "Gradle build - SUCCESS";
+fi
+
+cd e-arbiter-web;
+ng_output=$(ng build --env=dev --base-href "https://arturczopek.github.io/e-arbiter/" -aot); ng_return_code=$?
+
 # TODO uncomment, you know when..:)
 # - ng test --watch=false
+
+if (( ng_return_code != 0 ))
+then
+    echo "Ng build - !! FAIL !!";
+    exit 1;
+else
+    echo "Ng build - SUCCESS";
+    exit 0;
+fi
 
