312b27caa649d0eaa4498ad0f409fe72925643c5
Maciej Marczak <maciejmarczak@hotmail.com>
2017-07-15 23:48:00 +0200

    #69 - add validator configuration

---
+++ e-arbiter-tournament/src/main/java/pl/cyganki/tournament/configuration/ValidatorConfiguration.java
@@ -0,0 +1,21 @@
+package pl.cyganki.tournament.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ValidatorConfiguration {
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+}
