9dc77224bf8bda5823e9121bc5fe98de94f8148d
Maciej Marczak <maciejmarczak@hotmail.com>
2017-07-16 16:43:42 +0200

    #69 - add tournament adding endpoint

--- e-arbiter-tournament/src/main/java/pl/cyganki/tournament/controller/TournamentController.java
+++ e-arbiter-tournament/src/main/java/pl/cyganki/tournament/controller/TournamentController.java
@@ -1,12 +1,11 @@
 package pl.cyganki.tournament.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.cyganki.tournament.model.Tournament;
 import pl.cyganki.tournament.repository.TournamentRepository;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,6 +17,11 @@ public class TournamentController {
     @Autowired
     public TournamentController(TournamentRepository tournamentRepository) {
         this.tournamentRepository = tournamentRepository;
+    }
+
+    @PostMapping("/add")
+    public Tournament addTournament(@RequestBody @Valid Tournament tournament) {
+        return tournamentRepository.save(tournament);
     }
 
     @GetMapping("/test")
