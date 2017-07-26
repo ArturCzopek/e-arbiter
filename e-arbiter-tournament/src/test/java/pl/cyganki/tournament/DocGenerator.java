21a510d906c6c122cc0a97a122dc26dd16691043
Artur Czopek <arturcz32@gmail.com>
2017-07-26 17:52:32 +0200

    #51 - Added "debugging" for travis #71 - Moved test cfgs to cloud

--- e-arbiter-tournament/src/test/java/pl/cyganki/tournament/DocGenerator.java
+++ e-arbiter-tournament/src/test/java/pl/cyganki/tournament/DocGenerator.java
@@ -5,17 +5,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import pl.cyganki.utils.test.SwaggerTestUtilsKt;
 
-@WebAppConfiguration
+@SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {EArbiterTournamentApplication.class}, loader = SpringBootContextLoader.class)
 @ActiveProfiles("test")
