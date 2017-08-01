bc7e5b024d83682a678193ba7fbe58a4b8a43c82
Maciej Marczak <maciejmarczak@hotmail.com>
2017-08-01 23:29:48 +0200

    #70 - add name field to tournament form

--- e-arbiter-web/src/app/tournament/tournament-form.component.ts
+++ e-arbiter-web/src/app/tournament/tournament-form.component.ts
@@ -1,13 +1,31 @@
 import {FormBuilder, FormGroup} from "@angular/forms";
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Tournament} from "./interfaces/tournament.interface";
 
 @Component({
   selector: 'tournament-form',
   template: `
-    <h1>Works!</h1>
+    <form [formGroup]="myForm" class="ui form" (ngSubmit)="save(myForm.value)">
+      <h4 class="ui dividing header">Tournament</h4>
+      <div class="field">
+        <label>Name</label>
+        <input type="text" formControlName="name"/>
+      </div>
+    </form>
   `
 })
-export class TournamentFormComponent {
+export class TournamentFormComponent implements OnInit {
   public myForm: FormGroup;
+
   constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: ['']
+    });
+  }
+
+  private save(tournament: Tournament) {
+    console.log(tournament);
+  }
 }
