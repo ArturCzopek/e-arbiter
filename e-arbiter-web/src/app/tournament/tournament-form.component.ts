730931890d169e926c05c51abdb4ea1709f5e132
Maciej Marczak <maciejmarczak@hotmail.com>
2017-08-04 00:10:57 +0200

    #70 - add semantic-ui-calendar

--- e-arbiter-web/src/app/tournament/tournament-form.component.ts
+++ e-arbiter-web/src/app/tournament/tournament-form.component.ts
@@ -2,17 +2,32 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 import {Component, OnInit} from "@angular/core";
 import {Tournament} from "./interfaces/tournament.interface";
 
+declare var $: any;
+
 @Component({
   selector: 'tournament-form',
   template: `
     <form [formGroup]="myForm" class="ui form" (ngSubmit)="save(myForm.value)">
-      <h4 class="ui dividing header">Tournament</h4>
-      <div class="field">
-        <label>Name</label>
-        <input type="text" formControlName="name"/>
+      <h4 class="ui dividing header">Tworzenie Turnieju - Szablon</h4>
+      <div class="two fields">
+        <div class="field">
+          <label>Tytuł</label>
+          <input type="text" formControlName="name"/>
+        </div>
+        <div class="field">
+          <label>Do kiedy</label>
+          <div class="ui calendar" id="calendar">
+            <div class="ui input left icon">
+              <i class="calendar icon"></i>
+              <input type="text" formControlName="endDate">
+            </div>
+          </div>
+        </div>
       </div>
+      <button type="submit">Submit</button>
     </form>
-  `
+  `,
+  styleUrls: ['./tournament-form.scss']
 })
 export class TournamentFormComponent implements OnInit {
   public myForm: FormGroup;
@@ -20,8 +35,13 @@ export class TournamentFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    $('#calendar').calendar({
+      onChange: (date, text) => console.log(date, text)
+    });
+
     this.myForm = this.fb.group({
-      name: ['']
+      name: [''],
+      endDate: ['']
     });
   }
 
