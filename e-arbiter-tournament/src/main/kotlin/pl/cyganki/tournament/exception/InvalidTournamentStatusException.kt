3e36db937f3eb79e98e9ff33c86ac95007744518
Artur Czopek <arturcz32@gmail.com>
2017-08-04 12:59:21 +0200

    #83 - added tests for tournament (checking if states works properly)

--- e-arbiter-tournament/src/main/kotlin/pl/cyganki/tournament/exception/InvalidTournamentStatusException.kt
+++
@@ -1,6 +0,0 @@
-package pl.cyganki.tournament.exception
-
-import pl.cyganki.tournament.model.TournamentStatus
-
-class IllegalTournamentStatus(currentStatus: TournamentStatus, allowedStatuses: List<TournamentStatus>)
-    : IllegalStateException("Invalid tournament status! Found: $currentStatus, allowed: ${allowedStatuses.joinToString(separator = ", ")}")
