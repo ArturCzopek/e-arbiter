58ebcfeb1ecf4e7a93eb88579ae645fee4bdba4d
k2nder <k2nder@gmail.com>
2017-08-13 21:10:09 +0200

    Implemented fixes according to Artur's remarks.

--- e-arbiter-tournament/src/main/java/pl/cyganki/tournament/model/eventlistener/BeforeTournamentSaveListener.java
+++ e-arbiter-tournament/src/main/java/pl/cyganki/tournament/model/eventlistener/BeforeTournamentSaveListener.java
@@ -1,30 +1,52 @@
 package pl.cyganki.tournament.model.eventlistener;
 
 import com.mongodb.DBObject;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.BeforeSaveEvent;
 import org.springframework.stereotype.Component;
 import pl.cyganki.tournament.model.Tournament;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 @Component
 public class BeforeTournamentSaveListener extends AbstractMongoEventListener<Tournament> {
 
+    private static String PASSWORD_ENCODE_TYPE = "MD5";
+
+    @Value("${e-arbiter.salt}")
+    private int[] salts;
+
+    private byte[] salt;
+
     @Override
     public void onBeforeSave(BeforeSaveEvent<Tournament> event) {
         Tournament source = event.getSource();
 
-        if (source.getPassword() == null) {
+        //check if tournament is private - if not we escape from hashing
+        if (source.isPublicFlag()) {
             return;
+        }
+
+        if (source.getPassword() == null || StringUtils.isBlank(source.getPassword())) {
+            throw new RuntimeException("Password field in private tournaments cannot be blank or empty");
         }
 
         DBObject dbObject = event.getDBObject();
 
         String passwordToHash = source.getPassword();
-        // for now simple salt
-        byte[] salt = new byte[]{1, 2, 3, 4};
+
+        if (salts == null) {
+            // if we do not provide salts in configuration file we just choose a random two numbers
+            salts = new int []{1, 2};
+        }
+        else
+            salt = integersToByte(salts);
 
         String securePassword = getSecurePassword(passwordToHash, salt);
 
@@ -36,21 +58,36 @@ public class BeforeTournamentSaveListener extends AbstractMongoEventListener<Tou
         String generatedPassword = null;
 
         try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
+            MessageDigest md = MessageDigest.getInstance(PASSWORD_ENCODE_TYPE);
             md.update(salt);
             byte[] bytes = md.digest(passwordToHash.getBytes());
             StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++)
-            {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
 
+            for(int i=0; i< bytes.length ;i++) {
+                int widePosition = 0xff;
+                int moveIndex = 0x100;
+                sb.append(Integer.toString((bytes[i] & widePosition) + moveIndex, 16).substring(1));
+            }
             generatedPassword = sb.toString();
         }
         catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return generatedPassword;
+    }
+
+    private static byte [] integersToByte(int [] salts) {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream(salts.length * 4);
+            DataOutputStream dos = new DataOutputStream(bos);
+            for (int i = 0; i < salts.length; i++) {
+                dos.writeInt(salts[i]);
+            }
+
+            return bos.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- e-arbiter-tournament/src/main/java/pl/cyganki/tournament/utils/SampleDataLoader.java
+++ e-arbiter-tournament/src/main/java/pl/cyganki/tournament/utils/SampleDataLoader.java
@@ -3,6 +3,7 @@ package pl.cyganki.tournament.utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
