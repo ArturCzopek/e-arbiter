8f1deb24991dd93ab38dd264766f3d5100edd207
Maciej Marczak <maciejmarczak@hotmail.com>
2017-08-19 17:18:19 +0200

    #110 - fix ngSemantic issue

--- e-arbiter-web/src/app/dashboard/tournament-management-panel/tournament-management-panel.module.ts
+++ e-arbiter-web/src/app/dashboard/tournament-management-panel/tournament-management-panel.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {SharedModule} from "../../shared/shared.module";
 import {TournamentManagementPanelComponent} from "./tournament-management-panel.component";
-import {NgSemanticModule} from "ng-semantic";
+import {NgSemanticModule} from "ng-semantic/ng-semantic";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {TournamentFormComponent} from "./tournament-form.component";
 import {TaskModalComponent} from "./task-modal.component";
