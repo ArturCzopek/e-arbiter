386cdc99391297769685d1ff38f619c79aaea519
Artur Czopek <arturcz32@gmail.com>
2017-10-03 18:46:06 +0200

    #88 - Create dashboard opened tournaments page (#34)

--- e-arbiter-web/src/app/shared/service/tournament-preview.service.ts
+++ e-arbiter-web/src/app/shared/service/tournament-preview.service.ts
@@ -1,10 +1,10 @@
-import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
-import {Observable} from "rxjs/Observable";
-import {TournamentPreview} from "../interface/tournament-preview.interface";
-import {AuthService} from "./auth.service";
-import {environment} from "../../../environments/environment";
-import {Page} from "../interface/page.interface";
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {TournamentPreview} from '../interface/tournament-preview.interface';
+import {AuthService} from './auth.service';
+import {environment} from '../../../environments/environment';
+import {Page} from '../interface/page.interface';
 
 @Injectable()
 export class TournamentPreviewService {
@@ -13,7 +13,7 @@ export class TournamentPreviewService {
   }
 
 
-  public getUserActiveTournaments(pageNumber: number = 1, pageSize: number = 5, query: string = ""): Observable<Page<TournamentPreview>> {
+  public getUserActiveTournaments(pageNumber: number = 1, pageSize: number = 5, query: string = ''): Observable<Page<TournamentPreview>> {
 
     return this.http.get(
       `${environment.server.tournament.allTournamentsUrl}/active?page=${pageNumber - 1}&size=${pageSize}&query=${query}`,
@@ -23,10 +23,40 @@ export class TournamentPreviewService {
       .map(res => res.json());
   }
 
-  public getUserClosedTournaments(pageNumber: number = 1, pageSize: number = 5, query: string = ""): Observable<Page<TournamentPreview>> {
+  public getUserClosedTournaments(pageNumber: number = 1, pageSize: number = 5, query: string = ''): Observable<Page<TournamentPreview>> {
 
     return this.http.get(
       `${environment.server.tournament.allTournamentsUrl}/finished?page=${pageNumber - 1}&size=${pageSize}&query=${query}`,
+      this.authService.prepareAuthOptions()
+    )
+      .first()
+      .map(res => res.json());
+  }
+
+  public getNewestPublicTournaments(pageNumber: number = 1, pageSize: number = 5): Observable<Page<TournamentPreview>> {
+
+    return this.http.get(
+      `${environment.server.tournament.allTournamentsUrl}/newest?page=${pageNumber - 1}&size=${pageSize}`,
+      this.authService.prepareAuthOptions()
+    )
+      .first()
+      .map(res => res.json());
+  }
+
+  public getMostPopularPublicTournaments(pageNumber: number = 1, pageSize: number = 5): Observable<Page<TournamentPreview>> {
+
+    return this.http.get(
+      `${environment.server.tournament.allTournamentsUrl}/popular?page=${pageNumber - 1}&size=${pageSize}`,
+      this.authService.prepareAuthOptions()
+    )
+      .first()
+      .map(res => res.json());
+  }
+
+  public getAlmostEndedPublicTournaments(pageNumber: number = 1, pageSize: number = 5): Observable<Page<TournamentPreview>> {
+
+    return this.http.get(
+      `${environment.server.tournament.allTournamentsUrl}/ending?page=${pageNumber - 1}&size=${pageSize}`,
       this.authService.prepareAuthOptions()
     )
       .first()
