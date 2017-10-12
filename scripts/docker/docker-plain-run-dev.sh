8b2a25e8e0927324e2a4c47297d4dc238bfbdc1b
Artur Czopek <arturcz32@gmail.com>
2017-10-12 21:19:24 +0200

    - created docker container for mongo - fixed all issues (except connecting executor)

--- scripts/docker/docker-plain-run-dev.sh
+++ scripts/docker/docker-plain-run-dev.sh
@@ -27,13 +27,14 @@ cd $E_ARB_ROOT_FOLDER
 # separated network would be ok
 # volumes are persistent, dbs have default volumes in docker, so no problem with that approach to db
 
+# for getting logs from proper container type: docker logs --details [container-id]
 # develop client app should has a possibility to mount volume in system, command -v /path-where:/path-container
 
 docker container run -p ${E_ARB_CFG_PORT}:${E_ARB_CFG_PORT} --name e-arbiter-config-dev --env E_ARB_USR_PASS=$E_ARB_USR_PASS --network earbiter-dev --rm earbiterinfo/e-arbiter-config-dev:latest
 docker container run -p ${E_ARB_EUR_PORT}:${E_ARB_EUR_PORT} --name e-arbiter-eureka-dev --network earbiter-dev --rm earbiterinfo/e-arbiter-eureka-dev:latest
 docker container run -p ${E_ARB_API_PORT}:${E_ARB_API_PORT} --name e-arbiter-api-gateway-dev --network earbiter-dev --rm earbiterinfo/e-arbiter-api-gateway-dev:latest
+docker container run -p 192.168.0.1:${E_ARB_DEV_TOUR_MONGO_PORT}:27017 --name e-arbiter-mongo-dev --network earbiter-dev --rm earbiterinfo/e-arbiter-mongo-dev:latest
 
-# token should be passed only by envs there!!
 docker container run -p ${E_ARB_AUTH_PORT}:${E_ARB_AUTH_PORT} --name e-arbiter-auth-dev --env E_ARB_DEV_GH_CLIENT_ID=$E_ARB_DOCK_DEV_GH_CLIENT_ID --env E_ARB_DEV_GH_CLIENT_SECRET=$E_ARB_DOCK_DEV_GH_CLIENT_SECRET --network earbiter-dev --rm earbiterinfo/e-arbiter-auth-dev:latest
 docker container run -p ${E_ARB_TRES_PORT}:${E_ARB_TRES_PORT} --name e-arbiter-tour-res-dev --network earbiter-dev --rm earbiterinfo/e-arbiter-tour-res-dev:latest
 docker container run -p ${E_ARB_TOUR_PORT}:${E_ARB_TOUR_PORT} --name e-arbiter-tournament-dev --env E_ARB_TOUR_MAIL_PASSWORD=$E_ARB_USR_PASS --network earbiter-dev --rm earbiterinfo/e-arbiter-tournament-dev:latest
--- scripts/docker/dockerBuild-dev.sh
+++ scripts/docker/dockerBuild-dev.sh
@@ -9,6 +9,7 @@ docker build --file "e-arbiter-config/Dockerfile-dev" -t earbiterinfo/e-arbiter-
 docker build --file "e-arbiter-eureka/Dockerfile-dev" -t earbiterinfo/e-arbiter-eureka-dev:latest ./e-arbiter-eureka
 docker build --file "e-arbiter-auth/Dockerfile-dev" -t earbiterinfo/e-arbiter-auth-dev:latest ./e-arbiter-auth
 docker build --file "e-arbiter-api-gateway/Dockerfile-dev" -t earbiterinfo/e-arbiter-api-gateway-dev:latest ./e-arbiter-api-gateway
+docker build --file "e-arbiter-mongo/Dockerfile-dev" -t earbiterinfo/e-arbiter-mongo-dev:latest ./e-arbiter-mongo
 docker build --file "e-arbiter-tournament/Dockerfile-dev" -t earbiterinfo/e-arbiter-tournament-dev:latest ./e-arbiter-tournament
 docker build --file "e-arbiter-tournament-results/Dockerfile-dev" -t earbiterinfo/e-arbiter-tour-res-dev:latest ./e-arbiter-tournament-results
 docker build --file "e-arbiter-web/Dockerfile-dev" -t earbiterinfo/e-arbiter-web-dev:latest ./e-arbiter-web
