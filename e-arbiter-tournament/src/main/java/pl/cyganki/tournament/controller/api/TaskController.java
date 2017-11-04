ce0fa26e4301ab5ded0aac1141f3b8a424508415
Artur Czopek <arturcz32@gmail.com>
2017-11-04 13:12:44 +0100

    #97 - Add fetching results for tournament/task/task-user (#54)
    Populated results for active tournaments
    Populated results for finished tournaments
    Draft fetching tournament results.
    Fetching tournament results fixes.
    #97 - created first version of fetching all results for tournament
    #97 - changed methods for a new utils lib
    #97 - added fetching results to development card
    #97 - fixed auth endpoint for fetching users names
    #97 - moved some part of model to utils
    #97 - added tests for fetching results
    #97 - removed some results for fetching empty ones
    #97 - removed redundant controller
    - updated favicon

--- e-arbiter-tournament/src/main/java/pl/cyganki/tournament/controller/api/TaskController.java
+++ e-arbiter-tournament/src/main/java/pl/cyganki/tournament/controller/api/TaskController.java
@@ -1,6 +1,9 @@
 package pl.cyganki.tournament.controller.api;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.cyganki.tournament.model.CodeSubmitForm;
 import pl.cyganki.tournament.model.QuizSubmission;
 import pl.cyganki.tournament.model.Task;
